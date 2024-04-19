edca2820db00b19f23951338c150817050802e80
Moein Madadi <moeinmadadi2002@gmail.com>
2024-04-19 12:01:02 +0330

    adds print for test

--- est.py
+++ est.py
@@ -151,6 +151,8 @@ images_all = []
 ind_all = []
 for c in range(NC):
     ind = [correct[i] for i, label in enumerate(targets) if label == c]
+    print(f"len(correct): {len(correct)}")
+    print(f"len(targets): {len(targets)}")
     print(f"len(ind): {len(ind)}")
     print(f"NI: {NI}")
     ind = np.random.choice(ind, NI, replace=False)
